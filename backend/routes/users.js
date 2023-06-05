bf16a717db554ea64ccf807555fdfd09e1c40f7e
faisalaldarees <faisalaldarees@gmail.com>
2023-06-05 23:05:58 +0300

    minor fix

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -9,6 +9,7 @@ const {verifyToken, verifyTokenAuth, verifyTokenAndAdmin} = require('./verifyTok
 router.post("/register", async (req, res) => {
     try {
         const user = new User({
+            name: req.body.name,
             email: req.body.email,
             password: req.body.password,
         })
